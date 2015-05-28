ed1d0ee55d721496136ce1e20f8ce3cde1e2d9af
Dante Degl'Innocenti <dante082@gmail.com>
2015-05-28 12:30:55 +0200

    First commit

---
+++ src/italiansentiment/FeaturesMatrixGenerator.java
@@ -0,0 +1,103 @@
+/**
+ * Computes the features matrix and generete the ARFF (Weka) file
+ *
+ * LICENSE: To choose
+ *
+ * package: italianSentiment
+ * author: Dante Degl'Innocenti & Dario De Nart
+ * mail: deglinnocenti.dante@spes.uniud.it
+ * copyright: Dante Degl'Innocenti
+ *
+ * 2015/02/24 - DDI - v001: Initial structure of the class
+ */
+package italiansentiment;
+
+import au.com.bytecode.opencsv.CSV;
+import it.uniud.POSTaggerUtils.ItalianLinguisticUtils;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dante Degl'Innocenti
+ */
+public class FeaturesMatrixGenerator {
+
+    public static void main(String[] args) {
+        //Review SET
+//        String reviewDataset = "/home/dante/Dropbox/universita/dataset/tripadvisorITA/tripadvisorBALANCED.csv";
+        String reviewDataset = "/home/dante/Dropbox/universita/dataset/tripadvisorITA/validationSet.csv";
+        
+        Set<String> features = new HashSet<>();
+        
+//        String fileName = "/home/dante/Dropbox/universita/dottorato/corsiSeguiti/ANN/frequency/250.txt";
+        String fileName = "/home/dante/Dropbox/universita/dottorato/corsiSeguiti/ANN/frequency/250.txt";
+        String outputArffFile = "/home/dante/Scrivania/250_VALIDATION.arff";
+        BufferedReader br = null;
+
+        //read the features file
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(fileName));
+            while ((sCurrentLine = br.readLine()) != null) {
+                features.add(sCurrentLine); //insert each word in the hashset
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        ArrayList<String> featureList= new ArrayList<>();
+        featureList.addAll(features);
+        System.out.println(featureList.size());
+        ArffExporter myArffExporter = new ArffExporter(featureList);
+
+        //Now we have the features (words) set
+        CSV csv = CSV.separator(',').quote('"').create();
+        csv.read(reviewDataset, (int rowIndex, String[] values) -> {
+            assert values.length == 5;
+            String title = values[1];
+            String body = values[2];
+            String label = values[4];   //classification!
+            String allReview = title + " " + body;
+            allReview = allReview.toLowerCase();
+            //stemming and tokenization
+            ArrayList<String> stemmedReview = ItalianLinguisticUtils.italianStem(allReview);
+            HashMap<String, Double> reviewFeatures = new HashMap<>();
+            //for each word check its presence in the features set
+            System.out.print("Review " + (rowIndex + 1));
+            for (String word : stemmedReview) {
+                if (features.contains(word)) {
+                    reviewFeatures.put(word, 1.0);
+                }
+            }
+            myArffExporter.addElement(label, reviewFeatures);
+            System.out.println(" DONE");
+        });
+
+        System.out.println("Writing ARFF File...");
+        //Write arff file
+        try {
+            myArffExporter.writeSparseArffFile(new File(outputArffFile), Boolean.TRUE, Boolean.TRUE);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FeaturesMatrixGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
---
+++ src/italiansentiment/WordFrequency.java
@@ -0,0 +1,103 @@
+/**
+ * Computes the word frequency for tripadvisorITA dataset
+ *
+ * LICENSE: To choose
+ *
+ * package: italianSentiment
+ * author: Dante Degl'Innocenti
+ * mail: deglinnocenti.dante@spes.uniud.it
+ * copyright: Dante Degl'Innocenti
+ *
+ * 2015/02/17 - DDI - v001: Initial structure of the class
+ * 2015/02/19 - DDI - v002: Switched on opencsv library
+ */
+package italiansentiment;
+
+import au.com.bytecode.opencsv.CSV;
+import it.uniud.POSTaggerUtils.ItalianLinguisticUtils;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Prints the stemmed word frequency for tripadvisorITA dataset
+ *
+ * @author DDI
+ */
+public class WordFrequency {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // mappa parola -> lista di voti
+        HashMap<String, ArrayList<Float>> voteDictionary = new HashMap<>();
+
+        String file = "/home/dante/Dropbox/universita/dataset/tripadvisorITA/tripadvisorBALANCED.csv";
+        HashMap<String, Integer> wordFrequency = new HashMap<>();
+        ArrayList<Float> allvotes=new ArrayList<>();
+        CSV csv = CSV.separator(',').quote('"').create();
+        csv.read(file, (int rowIndex, String[] values) -> {
+            //here csv row logic
+            assert values.length == 5;
+            String title = values[1];
+            String body = values[2];
+            float vote = getVote(values[4]);
+            String allReview = title + " " + body;
+            allReview = allReview.toLowerCase();
+            allvotes.add(vote);
+            //stemming and tokenization
+            ArrayList<String> stemmedReview = ItalianLinguisticUtils.italianStem(allReview);
+
+            //inserting into the counting map
+            for (String word : stemmedReview) {
+                if (wordFrequency.get(word) == null) {
+                    //word not in hashmap
+                    wordFrequency.put(word, 1);
+                    ArrayList<Float> voteList = new ArrayList<>();
+                    voteList.add(vote);
+                    voteDictionary.put(word, voteList);
+                } else {
+                    //word in hashmap. We need to increment the count value
+                    wordFrequency.put(word, 1 + wordFrequency.get(word));
+                    ArrayList<Float> voteList = voteDictionary.get(word);
+                    voteList.add(vote);
+                    voteDictionary.put(word, voteList);
+                }
+            }
+
+        });
+        Float datasetAverage = getAverage(allvotes);
+//        Tagger myTagger = new Tagger("/home/dante/MEGA/NetBeansProjects/POSTagger/src/resources/post.db");
+        //print
+        for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            Float wordAverage = getAverage(voteDictionary.get(key));
+            System.out.println(key + "," + value + "," + Math.abs(datasetAverage-wordAverage)  + "," + value*Math.abs(datasetAverage-wordAverage) );
+        }
+
+    }
+    
+    public static float getAverage(List<Float> votes){
+        float acc = 0;
+        for(Float i:votes){
+            acc+=i;
+        }
+        return acc/(float)votes.size();
+    }
+
+    public static float getVote(String vote) {
+        switch (vote) {
+            case "positive":
+                return 1;
+            case "neutral":
+                return 0;
+            case "negative":
+                return -1;
+            default:
+                return 0;
+        }
+    }
+}
